c53fcf2dd9684a4d2b2144fe627ce2a459b60372
Okan <okany4404@gmail.com>
2023-12-27 01:22:19 +0300

    Add - the backend connection to the Northwind database has been established using Axios.

---
+++ src/layouts/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container, Menu } from 'semantic-ui-react'
+
+export default function Categories() {
+    return (
+        <div>
+            <Container>
+                <Menu pointing vertical>
+                    <Menu.Item
+                        name='home'
+                    />
+                    <Menu.Item
+                        name='messages'
+                    />
+                    <Menu.Item
+                        name='friends'
+                    />
+                </Menu>
+            </Container>
+        </div>
+    )
+}
